aa7cd1ad9d4bde2ab36d8cd4d44e7a281663e8ca
EffBureau <francis.bureau2@gmail.com>
2024-05-08 23:04:18 -0400

    Initial commit

---
+++ Source/Ropes/Ropes.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ropes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ropes, "Ropes" );
+ 
---
+++ Source/Ropes/RopesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RopesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARopesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARopesGameMode();
+};
+
+
+
